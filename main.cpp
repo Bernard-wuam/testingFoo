e1ac37af6a934ece8f2a04ebd257437e673ffabe
Bernard-wuam <wuambernard@yahoo.com>
2026-02-08 03:07:22 -0800

    added a comment in the main..

--- main.cpp
+++ main.cpp
@@ -2,9 +2,11 @@
 #include <boost/json.hpp>
 #include <boost/json/value.hpp>
 
-int main(int, char**){
+int main(int, char **)
+{
+    // example to show this code should be used.
     boost::json::value val = boost::json::object{{"name", "frankwhite"}};
     std::string stringVal{boost::json::serialize(val)};
-    std::cout << " this is the value of the json: ==> "  << stringVal << std::endl;
+    std::cout << " this is the value of the json: ==> " << stringVal << std::endl;
     std::cout << "Hello, from learningcxxlibinstallation!\n";
 }
